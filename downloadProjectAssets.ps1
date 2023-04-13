d0237035a69672cb2cd01dc79b206590aba88452
桑原 崚介 <kuwabara.ryosuke@nsg.gr.jp>
2023-04-13 09:25:01 +0900

    バッチファイルを直しました。

--- downloadProjectAssets.ps1
+++ downloadProjectAssets.ps1
@@ -1,5 +1,29 @@
-$serverDir = [System.Environment]::GetEnvironmentVariable("PROJECT_ASSETS_SERVER_DIR", "Machine")
-$localDir = [System.Environment]::GetEnvironmentVariable("PROJECT_ASSETS_LOCAL_DIR", "Machine")
+$shellPath = Get-Location
+
+$scriptsDirPath = Join-Path $shellPath "\Assets\Scripts"
+$resourcesDirPath = Join-Path $shellPath "\Assets\Resources"
+$projectAssetsDirPath = Join-Path $resourcesDirPath "\ProjectAssets"
+
+if( Test-Path $scriptsDirPath ){
+
+}else{
+    New-Item $scriptsDirPath -ItemType Directory
+}
+
+if( Test-Path $resourcesDirPath ){
+
+}else{
+    New-Item $resourcesDirPath -ItemType Directory
+}
+
+if( Test-Path $projectAssetsDirPath ){
+
+}else{
+    New-Item $projectAssetsDirPath -ItemType Directory
+}
+
+$serverDir = "\\201b05\ShareFolder\SampleGame\JJC_Minecraft\ProjectAssets"
+$localDir = $projectAssetsDirPath
 $isEnable = $TRUE
 
 if(Test-Path -Path $serverDir){
--- prepare.ps1
+++ prepare.ps1
@@ -1,6 +1,4 @@
 $shellPath = Get-Location
-[System.Environment]::SetEnvironmentVariable("JJC_JUGYO_PJ_ROOT", $shellPath, "Machine")
-
 $scriptsDirPath = Join-Path $shellPath "\Assets\Scripts"
 $resourcesDirPath = Join-Path $shellPath "\Assets\Resources"
 $projectAssetsDirPath = Join-Path $resourcesDirPath "\ProjectAssets"
@@ -22,6 +20,3 @@ if( Test-Path $projectAssetsDirPath ){
 }else{
     New-Item $projectAssetsDirPath -ItemType Directory
 }
-
-[System.Environment]::SetEnvironmentVariable("JJC_JUGYO_PJ_ASSETS_L_DIR", $projectAssetsDirPath, "Machine")
-[System.Environment]::SetEnvironmentVariable("JJC_JUGYO_PJ_ASSETS_S_DIR", "\\201b05\ShareFolder\2023\PROGRAM_JUGYO\JJC_Minecraft\ProjectAssets", "Machine")
